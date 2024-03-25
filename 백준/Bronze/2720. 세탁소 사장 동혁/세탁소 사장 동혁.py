c2a84c26c2faa1a85133d4817bdfe02d13bcdf26
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-03-25 16:43:19 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 44 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -1,8 +1,9 @@
-n = int(input())
+import sys
+n = int(sys.stdin.readline())
 coins = [25, 10, 5, 1]
 
 for _ in range(n):
-  charge = int(input())
+  charge = int(sys.stdin.readline())
   result = [0, 0, 0, 0]
   for i in range(len(coins)):
     result[i] = charge // coins[i]

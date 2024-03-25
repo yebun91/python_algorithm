4f466e6a020d7323e4cbb1b6d7543ca192ed864a
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-03-25 16:39:03 +0900

    [Bronze III] Title: 세탁소 사장 동혁, Time: 88 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2720. 세탁소 사장 동혁/세탁소 사장 동혁.py
@@ -0,0 +1,11 @@
+n = int(input())
+coins = [25, 10, 5, 1]
+
+for _ in range(n):
+  charge = int(input())
+  result = [0, 0, 0, 0]
+  for i in range(len(coins)):
+    result[i] = charge // coins[i]
+    charge = charge % coins[i]
+  
+  print(*result)

0618ff90c6ef7da4af22902b1dae5ef447c5a1be
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-03-27 11:50:21 +0900

    [Silver V] Title: 이장님 초대, Time: 156 ms, Memory: 123220 KB -BaekjoonHub

--- 백준/Silver/9237. 이장님 초대/이장님 초대.py
+++ 백준/Silver/9237. 이장님 초대/이장님 초대.py
@@ -2,14 +2,14 @@ n = int(input())
 t = list(map(int, input().split()))
 t.sort()
 
-day = t[n-1] + 1
+day = t[n-1]
 result = 0
 
 while day > 0 or len(t) > 0:
   if len(t) > 0 :
-    tree = t.pop() + 1 
+    tree = t.pop()
     if tree > day : day = tree 
   day -= 1 
   result += 1 
 
-print(result + 1) # 마지막 나무가 자란 다음 날
+print(result + 2) # 마지막 나무가 자란 다음 날 + 심고난 다음날부터 자라는 나무

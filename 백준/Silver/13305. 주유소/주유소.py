444cfe20fd2b8addc408d5e2563b9d3a02a6898e
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-03-27 16:53:10 +0900

    [Silver III] Title: 주유소, Time: 116 ms, Memory: 45892 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.py
+++ 백준/Silver/13305. 주유소/주유소.py
@@ -2,15 +2,13 @@ n = int(input())
 r = list(map(int, input().split()))
 p = list(map(int, input().split()))
 
-# 걍 계속 내고 가다가 제일 싼 곳이 나오면 가득 채워서 쭉 가면 됨
-
-all = [[a, b] for a, b in zip(r, p[:-1])]
-minIndex = min(range(len(all)), key=lambda i: all[i][1])
 result = 0
-for i in range(n-1):
-  if i >= minIndex :
-    result += all[i][0] * all[minIndex][1]
-  else:
-    result += all[i][0] * all[i][1]
+min_p = p[0]
 
+for i in range(n-1) : 
+  # 현재 가격이 min_p보다 쌀 경우 min_p 갱신
+  if p[i] < min_p:
+    min_p = p[i]
+  result += min_p * r[i] 
+  # 만나는 p[i] 값마다 min_p와 비교하면서 적으면 갱신하는 방법으로 계속 더해나감
 print(result)

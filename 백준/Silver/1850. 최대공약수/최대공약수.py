9bdb83d624e71b9751c39bc915b87c174fe72c42
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-03-29 15:07:06 +0900

    [Silver I] Title: 최대공약수, Time: 48 ms, Memory: 40884 KB -BaekjoonHub

--- 백준/Silver/1850. 최대공약수/최대공약수.py
+++ 백준/Silver/1850. 최대공약수/최대공약수.py
@@ -1,13 +1,11 @@
 import sys
 input = lambda: sys.stdin.readline().strip()
 
+n, m = map(int, input().split())
+
 def gcd(a, b):
-    while b != 0:
-        a, b = b, a % b
-    return a
+  while b != 0:
+    a, b = b, a % b
+  return a
 
-n, m = map(int, input().split())  # 입력 받기
-gcd_value = gcd(n, m)  # 최대공약수 구하기
-
-result = '1' * gcd_value  # 최대공약수만큼의 1로 이루어진 문자열 생성
-print(result)  # 결과 출력
+print('1' * gcd(n,m))

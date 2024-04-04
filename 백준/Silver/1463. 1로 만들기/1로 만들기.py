14e38fa7bb0aefd95d943db72c958c0dcee0cb22
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-04-04 16:29:41 +0900

    [Silver III] Title: 1로 만들기, Time: 64 ms, Memory: 34036 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 n = int(input())
 
 # def dpFun(n):
@@ -17,17 +19,38 @@ n = int(input())
 #       dp = newDp
 #     result += 1
 
+# def dpFun(n):
+#   dp = [0] * (n + 1) # 0부터 n까지의 숫자를 인텍스로 사용하기 위함
+
+#   for i in range(2, n + 1):
+#     # 1을 뺀 경우와 비교
+#     dp[i] = dp[i - 1] + 1
+#     if i % 2 == 0:
+#       dp[i] = min(dp[i], dp[i // 2] + 1)
+#     if i % 3 == 0:
+#       dp[i] = min(dp[i], dp[i // 3] + 1)
+
+#   return dp[n]
+
 def dpFun(n):
-  dp = [0] * (n + 1) # 0부터 n까지의 숫자를 인텍스로 사용하기 위함
+  queue = deque([(n, 0)]) 
+  visited = set([n])
 
-  for i in range(2, n + 1):
-    # 1을 뺀 경우와 비교
-    dp[i] = dp[i - 1] + 1
-    if i % 2 == 0:
-      dp[i] = min(dp[i], dp[i // 2] + 1)
-    if i % 3 == 0:
-      dp[i] = min(dp[i], dp[i // 3] + 1)
+  while queue:
+    number, count = queue.popleft()
+    if number == 1:
+      return count
 
-  return dp[n]
+    if number % 3 == 0 and number // 3 not in visited:
+      visited.add(number // 3)
+      queue.append((number // 3, count + 1))
+    
+    if number % 2 == 0 and number // 2 not in visited:
+      visited.add(number // 2)
+      queue.append((number // 2, count + 1))
+    
+    if number - 1 not in visited:
+      visited.add(number - 1)
+      queue.append((number - 1, count + 1))
 
 print(dpFun(n))

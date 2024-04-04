af4f043180679c42c2c997c8f5de565af6abcb5c
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-04-04 16:26:19 +0900

    [Silver III] Title: 1로 만들기, Time: 360 ms, Memory: 38932 KB -BaekjoonHub

--- 백준/Silver/1463. 1로 만들기/1로 만들기.py
+++ 백준/Silver/1463. 1로 만들기/1로 만들기.py
@@ -1,19 +1,33 @@
 n = int(input())
 
-def dpFun():
-  result = 0
-  dp = [n]
-  while dp:
-    newDp = []
-    for number in dp:
-      if number == 1:
-        return result
-      if number % 3 == 0:
-        newDp.append(number // 3)
-      if number % 2 == 0:
-        newDp.append(number // 2)
-      newDp.append(number - 1)
-      dp = newDp
-    result += 1
+# def dpFun(n):
+#   result = 0
+#   dp = [n]
+#   while dp:
+#     print(dp)
+#     newDp = []
+#     for number in dp:
+#       if number == 1:
+#         return result
+#       if number % 3 == 0:
+#         newDp.append(number // 3)
+#       if number % 2 == 0:
+#         newDp.append(number // 2)
+#       newDp.append(number - 1)
+#       dp = newDp
+#     result += 1
 
-print(dpFun())
+def dpFun(n):
+  dp = [0] * (n + 1) # 0부터 n까지의 숫자를 인텍스로 사용하기 위함
+
+  for i in range(2, n + 1):
+    # 1을 뺀 경우와 비교
+    dp[i] = dp[i - 1] + 1
+    if i % 2 == 0:
+      dp[i] = min(dp[i], dp[i // 2] + 1)
+    if i % 3 == 0:
+      dp[i] = min(dp[i], dp[i // 3] + 1)
+
+  return dp[n]
+
+print(dpFun(n))

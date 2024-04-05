31aea27196edbba208d59c81abb5b7983322f76c
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-04-05 14:15:49 +0900

    [Silver I] Title: 정수 삼각형, Time: 156 ms, Memory: 40764 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.py
@@ -0,0 +1,20 @@
+import sys
+input = lambda: sys.stdin.readline().strip()
+
+n = int(input()) 
+numbers = [list(map(int, input().split())) for _ in range(n)]  
+
+dp = [[0 for _ in range(len(numbers[i]))] for i in range(n)]
+dp[0] = numbers[0]
+
+for i in range(1, n):
+  for j in range(len(numbers[i])):
+    if j == 0:
+      # 왼족 끝일 경우 더하는 경우의 수가 하나 뿐임
+      dp[i][j] = dp[i-1][j] + numbers[i][j]
+    elif j == len(numbers[i]) -1:
+      # 오른쪽 끝도 마찬가지!
+      dp[i][j] = dp[i-1][j-1] + numbers[i][j]
+    else:
+      dp[i][j] = max(dp[i-1][j], dp[i-1][j-1]) + numbers[i][j]
+print(max(dp[n-1]))

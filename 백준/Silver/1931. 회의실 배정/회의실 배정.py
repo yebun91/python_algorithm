c1b86df0cef7cafea99655fe36eab7cea41a7d40
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-04-09 10:33:13 +0900

    [Silver I] Title: 회의실 배정, Time: 4288 ms, Memory: 59852 KB -BaekjoonHub

---
+++ 백준/Silver/1931. 회의실 배정/회의실 배정.py
@@ -0,0 +1,13 @@
+n = int(input())
+m = [list(map(int, input().split())) for _ in range(n)]
+
+sortM = sorted(m, key=lambda x: (x[1], x[0]))
+result = 0
+t = 0
+
+for i in range(n):
+  if t <= sortM[i][0]:
+    result += 1
+    t = sortM[i][1]
+  
+print(result)

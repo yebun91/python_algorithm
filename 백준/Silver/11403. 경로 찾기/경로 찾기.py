5d43c490492eda6f3a612b800c9d1e8a8d8cfcdf
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-04-16 15:19:24 +0900

    [Silver I] Title: 경로 찾기, Time: 216 ms, Memory: 31120 KB -BaekjoonHub

--- 백준/Silver/11403. 경로 찾기/경로 찾기.py
+++ 백준/Silver/11403. 경로 찾기/경로 찾기.py
@@ -1,15 +1,11 @@
 n = int(input())
 graph = [list(map(int, input().split())) for _ in range(n)]
 
-for i in range(n):
-  graph[i][i] = 0
-
 for k in range(n):
   for i in range(n):
     for j in range(n):
       if graph[i][k] and graph[k][j]:
         graph[i][j] = 1
-
 
 for row in graph:
   print(*row)

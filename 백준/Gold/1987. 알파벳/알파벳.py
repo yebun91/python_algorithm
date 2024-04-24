27f56adc909dc1c820c603b16c81585dff43c2d9
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-04-24 15:08:27 +0900

    [Gold IV] Title: 알파벳, Time: 5764 ms, Memory: 178400 KB -BaekjoonHub

---
+++ 백준/Gold/1987. 알파벳/알파벳.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+input = lambda: sys.stdin.readline().strip()
+
+r, c = map(int, input().split())
+graph = [list(input()) for _ in range(r)]
+visited = [False] * 26  # 알파벳 방문 여부 체크
+max_length = 0
+
+dx = [0, 0, -1, 1]
+dy = [1, -1, 0, 0]
+
+def dfs(x, y, depth):
+  global max_length
+  max_length = max(depth, max_length)
+  if max_length == 26:
+    return
+  for i in range(4):
+    mx, my = x + dx[i], y + dy[i]
+    if 0 <= mx < r and 0 <= my < c:
+      index = ord(graph[mx][my]) - ord('A')
+      if not visited[index]:
+        visited[index] = True
+        dfs(mx, my, depth + 1)
+        visited[index] = False
+
+index = ord(graph[0][0]) - ord('A')
+visited[index] = True
+
+dfs(0, 0, 1)
+
+print(max_length)

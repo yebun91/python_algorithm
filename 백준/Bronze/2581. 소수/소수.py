fde22d6bf4481ce31796343751bf3bf1aaaa05d1
yebun91 <65695703+yebun91@users.noreply.github.com>
2024-11-17 19:19:48 +0900

    [Bronze II] Title: 소수, Time: 320 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/2581. 소수/소수.py
@@ -0,0 +1,20 @@
+def find_prime(M, N):
+    result = []
+
+    for number in range(M, N+1):
+        if number > 1:
+            for i in range(2, number):
+                if number % i == 0:
+                    break
+
+            else: result.append(number)
+
+    if len(result) == 0: print(-1)
+    else:
+        print(sum(result))
+        print(min(result))
+
+M = int(input())
+N = int(input())
+
+find_prime(M, N)
